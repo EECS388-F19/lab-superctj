b6b9542ea478f6922cdad10a96f902345eb13f72
superctj <congtj@umich.edu>
2019-09-09 15:33:09 -0400

    Lab 0

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import random
+
+if __name__=="__main__":
+    print("Tianji")
+    
+    x = random.randint(1, 101)
+    y = random.randint(1, 101)
+
+    print(x)
+    print(y)
+    print(f"Sum = {x+y}")
+    print(f"Average = {(x+y)/2}")
